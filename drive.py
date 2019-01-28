2108d2568d233c89eb3ac3c0371f5dd200a289f6
Patrick Gebert <patrick.gebert@web.de>
2019-01-28 20:57:59 +0100

    Init setup for driving simulation communication.

---
+++ drive.py
@@ -0,0 +1,66 @@
+#parsing command line arguments
+import argparse
+#decoding camera images
+import base64
+#for frametimestamp saving
+from datetime import datetime
+#reading and writing files
+import os
+#high level file operations
+import shutil
+#matrix math
+import numpy as np
+#real-time server
+import socketio
+#concurrent networking 
+import eventlet
+#web server gateway interface
+import eventlet.wsgi
+#image manipulation
+from PIL import Image
+#web framework
+from flask import Flask
+#input output
+from io import BytesIO
+
+
+
+#initialize our server
+sio = socketio.Server()
+#our flask (web) app
+app = Flask(__name__)
+
+#registering event handler for the server
+@sio.on('telemetry')
+def telemetry(sid, data):
+    if data:
+        # The current steering angle of the car
+        steering_angle = 0
+        # The current throttle of the car, how hard to push peddle
+        throttle = 1.0
+
+        send_control(steering_angle, throttle)
+
+@sio.on('connect')
+def connect(sid, environ):
+    print("connect ", sid)
+    send_control(0, 0)
+
+
+def send_control(steering_angle, throttle):
+    sio.emit(
+        "steer",
+        data={
+            'steering_angle': steering_angle.__str__(),
+            'throttle': throttle.__str__()
+        },
+        skip_sid=True)
+
+
+if __name__ == '__main__':
+    
+    # wrap Flask application with engineio's middleware
+    app = socketio.Middleware(sio, app)
+
+    # deploy as an eventlet WSGI server
+    eventlet.wsgi.server(eventlet.listen(('', 4567)), app)
